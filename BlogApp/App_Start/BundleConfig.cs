e375ce2b99ebff38736fc0b0b55aacbaa3e5126b
Miguel Roman <miguelerm@gmail.com>
2013-04-09 11:50:54 -0600

    Se crearon los bundles para los scripts

---
+++ BlogApp/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace BlogApp
+{
+    public class BundleConfig
+    {
+        public static void RegisterRoutes(BundleCollection bundles)
+        {
+
+            var appScripts = new ScriptBundle("~/bundles/appscripts");
+
+            appScripts.Include("~/Scripts/modernizr-{version}.js");
+            appScripts.Include("~/Scripts/jquery-{version}.js");
+            appScripts.Include("~/Scripts/jquery.validate.js");
+            appScripts.Include("~/Scripts/jquery.validate.unobtrusive.js");
+            appScripts.Include("~/Scripts/jquery.unobtrusive-ajax.js");
+
+            bundles.Add(appScripts);
+
+        }
+    }
+}
